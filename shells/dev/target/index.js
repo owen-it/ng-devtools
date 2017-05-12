b19969b42ae530646b4aa311b850514ff3d76df1
Antério Vieira da Silva <anteriovieira@gmail.com>
2017-05-12 16:52:47 -0300

    Updated

--- shells/dev/target/index.js
+++ shells/dev/target/index.js
@@ -3,14 +3,14 @@ import * as angular from 'angular'
 import counter from './Counter.ng'
 import events from './Events.ng'
 
-angular.module('app.count', []).components({ counter, events });
+angular.module('app.count', [])
+.components({ counter, events })
+.value('counter', { total: 54 });
+
+
 angular.module('app', ['app.count'])
-
 .info({version: angular.version })
-
 .constant('APP_NAME', 'Angular DevTools')
 .constant('APP_VERSION', '0.0.1')
-
-.value('counter', { total: 54 })
 
 angular.bootstrap(document.body, ['app'])
